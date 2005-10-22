073ff145b91737266cc2a16cfc5400b6d5ea1511
Dave Cross <dave@dave.org.uk>
2005-10-22 21:29:58 +0000

    Added pod coverage tests.

--- lib/Tie/Hash/FixedKeys.pm
+++ lib/Tie/Hash/FixedKeys.pm
@@ -148,9 +148,12 @@ modify it under the same terms as Perl itself.
 perl(1), perltie(1).
 
 =cut
-
+   
 #
 # $Log$
+# Revision 1.9  2005/10/22 21:29:58  dave
+# Added pod coverage tests.
+#
 # Revision 1.8  2004/10/23 16:05:19  dave
 # Improved test coverage.
 #
