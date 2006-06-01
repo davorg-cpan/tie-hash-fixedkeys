ac2e75d4195dcc3cd0001d5252f5ef8d79ad2e04
Dave Cross <dave@dave.org.uk>
2006-06-01 18:59:12 +0000

    Added note about Hash::Util::lock_keys.

--- lib/Tie/Hash/FixedKeys.pm
+++ lib/Tie/Hash/FixedKeys.pm
@@ -49,6 +49,15 @@ in the list. These values are initialised to C<undef> when the hash is
 tied. If you try to C<delete> one if the keys, the effect is that the
 value is reset to C<undef>.
 
+=head2 NOTE
+
+Versions of Perl from 5.8.0 include a module called L<Hash::Util> which
+contains a function called C<lock_keys> which does the same as this module
+but in a faster and more powerful way. I recommend that you use that 
+method in place of this module.
+
+This module is left on CPAN as an example of tied hashes.
+
 =cut
 
 package Tie::Hash::FixedKeys; 
@@ -134,7 +143,7 @@ __END__
 
 =head1 AUTHOR
 
-Dave Cross <dave@dave.org.uk>
+Dave Cross <dave@mag-sol.com>
 
 =head1 COPYRIGHT
 
@@ -151,6 +160,9 @@ perl(1), perltie(1).
    
 #
 # $Log$
+# Revision 1.10  2006/06/01 18:59:12  dave
+# Added note about Hash::Util::lock_keys.
+#
 # Revision 1.9  2005/10/22 21:29:58  dave
 # Added pod coverage tests.
 #
